d16952e5baf8cddb8abc800bd736937f6a1bc58f
LuKe2_Ink <56109204+LuKe2Ink@users.noreply.github.com>
2022-02-05 19:17:43 +0100

    cancel
    Added the cancel feature

--- calculator.js
+++ calculator.js
@@ -1,25 +1,52 @@
+
+var number=[0,0];
+var ans=0;
+var decimal=false;
+var decimalPosition=-1;
+var index=0;
+
 $(document).ready(function(){
-    var firstNumber=0;
-    var secondNumber=0;
-    var ans=0;
-    var decimal=false;
-    var decimalPosition=-1;
 
     $("td").click(function(){
         if($(this).text() != "C" && $(this).text() != "."){
             if(decimal){
-                firstNumber += (parseFloat($(this).text())*(10**decimalPosition));
+                number[index] += (parseFloat($(this).text()*(10**decimalPosition)));
+                $("#display").text(number[index].toFixed(-decimalPosition));
                 decimalPosition--;
-                $("#display").text(firstNumber);
 
             } else {
-                firstNumber = firstNumber*10 + parseInt($(this).text());
-                $("#display").text(firstNumber);
+                number[index] = number[index]*10 + parseInt($(this).text());
+                $("#display").text(number[index]);
             }
         }
     });
 
     $("#decimal").click(function(){
         decimal = true;
+        number[index] = parseFloat(number[index]);
+    });
+
+    $("#cancel").click(function(){
+        number = [0,0];
+        index=0;
+        decimal=false;
+        decimalPosition=-1;
+        $("#display").text(number[index]);
+    });
+
+    $("#divide").click(function(){
+
+    });
+
+    $("#multiply").click(function(){
+    });
+
+    $("#subtract").click(function(){
+    });
+
+    $("#addition").click(function(){
+    });
+
+    $("#equal").click(function(){
     });
 });
