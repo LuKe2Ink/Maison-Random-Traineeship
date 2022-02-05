a8faf2535b7753a7cd5ca4f2188c62ee4738904d
LuKe2_Ink <56109204+LuKe2Ink@users.noreply.github.com>
2022-02-05 18:37:00 +0100

    update
    Updated some error and added a integer and decimal number display

--- calculator.js
+++ calculator.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
-    
+    var firstNumber=0;
+    var secondNumber=0;
+    var ans=0;
+    var decimal=false;
+    var decimalPosition=-1;
+
+    $("td").click(function(){
+        if($(this).text() != "C" && $(this).text() != "."){
+            if(decimal){
+                firstNumber += (parseFloat($(this).text())*(10**decimalPosition));
+                decimalPosition--;
+                $("#display").text(firstNumber);
+
+            } else {
+                firstNumber = firstNumber*10 + parseInt($(this).text());
+                $("#display").text(firstNumber);
+            }
+        }
+    });
+
+    $("#decimal").click(function(){
+        decimal = true;
+    });
 });
