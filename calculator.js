1c20eef7052050a438091c6519c637fa3aa49c51
LuKe2_Ink <56109204+LuKe2Ink@users.noreply.github.com>
2022-02-06 09:52:37 +0100

    Added operation
    Fixed a misclick in html and added modular operation

--- calculator.js
+++ calculator.js
@@ -1,14 +1,62 @@
+function result() {
+    return new Function('return ' + number[0] + operate + number[1])();
+}
+
+function equal() {
+    if(index == 2){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function reset() {
+    ans = false;
+    number = [0,0];
+    index=0;
+    decimal=false;
+    decimalPosition=-1;
+    $("#display").text(number[index]);
+}
+
+function operator(operation){
+    if(!alreadyClicked){
+        alreadyClicked = true;
+        index++;
+        if(!equal()){
+            operate=operation;
+            $("#display").text(0);
+        } else {
+            number[0] = result();
+            console.log(number[0]);
+            number[1] = 0;
+            operate=operation;
+            $("#display").text(number[0]);
+        }
+    }
+}
 
 var number=[0,0];
-var ans=0;
+var ans=false;
 var decimal=false;
 var decimalPosition=-1;
 var index=0;
+var operate="";
+var alreadyClicked=false;
+
 
 $(document).ready(function(){
 
     $("td").click(function(){
-        if($(this).text() != "C" && $(this).text() != "."){
+
+        alreadyClicked = false;
+        
+        if(equal()){
+            index=1;
+            $("#display").text(number[index]);
+        }
+
+        if($(this).text() != "C" && $(this).text() != "." ){
             if(decimal){
                 number[index] += (parseFloat($(this).text()*(10**decimalPosition)));
                 $("#display").text(number[index].toFixed(-decimalPosition));
@@ -27,26 +75,26 @@ $(document).ready(function(){
     });
 
     $("#cancel").click(function(){
-        number = [0,0];
-        index=0;
-        decimal=false;
-        decimalPosition=-1;
-        $("#display").text(number[index]);
+        reset();
     });
 
     $("#divide").click(function(){
-
+        operator("/");
     });
 
     $("#multiply").click(function(){
+        operator("*");
     });
 
     $("#subtract").click(function(){
+        operator("-");
     });
 
     $("#addition").click(function(){
+        operator("+");
     });
 
     $("#equal").click(function(){
+
     });
 });
